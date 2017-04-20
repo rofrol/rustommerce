66e2472aecb61e90d3edd77ee2b0ca88d80b2ed8
Roman Frołow <rofrol@gmail.com>
2017-04-20 19:42:19 +0200

    path.resolve

--- client/tools/browsersync.js
+++ client/tools/browsersync.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
-var root = __dirname + '/../..';
+var path = require('path');
+var root = path.resolve(__dirname + '/../..');
 require('dotenv').config({ path: root + '/.env' })
 var browserSync = require('browser-sync');
 
+var dist = path.resolve(root + '/client/dist')
 browserSync({
   proxy: 'localhost:' + process.env.static_port,
   serveStatic: [],
-  files: [root + '/client/dist'],
+  files: [dist],
   open: false,
   port: process.env.browsersync_port,
   reloadDebounce: 1000
