08d492a5387cc71153bf41aaa2ac4378e913f1b0
Roman Frołow <rofrol@gmail.com>
2017-04-20 19:42:28 +0200

    CORS

--- server/src/files.rs
+++ server/src/files.rs
@@ -3,32 +3,34 @@ use rocket::response::NamedFile;
 use std::io;
 use std::path::{Path, PathBuf};
 
+use cors::CORS;
+
 #[get("/")]
-fn index() -> io::Result<NamedFile> {
-    NamedFile::open("../client/dist/index.html")
+fn index() -> CORS<io::Result<NamedFile>> {
+    CORS::any(NamedFile::open("../client/dist/index.html"))
 }
 
 // http://stackoverflow.com/questions/2208933/how-do-i-force-a-favicon-refresh
 #[allow(unused_variables)]
 #[get("/favicon.ico?<v>")]
-fn favicon(v: V) -> io::Result<NamedFile> {
-    NamedFile::open("../client/dist/favicon.ico")
+fn favicon(v: V) -> CORS<io::Result<NamedFile>> {
+    CORS::any(NamedFile::open("../client/dist/favicon.ico"))
 }
 
 #[allow(unused_variables)]
 #[get("/<file..>", rank = 100)]
-fn redirect_to_index(file: PathBuf) -> io::Result<NamedFile> {
-    NamedFile::open("../client/dist/index.html")
+fn redirect_to_index(file: PathBuf) -> CORS<io::Result<NamedFile>> {
+    CORS::any(NamedFile::open("../client/dist/index.html"))
 }
 
 #[get("/js/<file..>")]
-fn js(file: PathBuf) -> io::Result<NamedFile> {
-    NamedFile::open(Path::new("../client/dist/js").join(file))
+fn js(file: PathBuf) -> CORS<io::Result<NamedFile>> {
+    CORS::any(NamedFile::open(Path::new("../client/dist/js").join(file)))
 }
 
 #[get("/styles/<file..>")]
-fn styles(file: PathBuf) -> io::Result<NamedFile> {
-    NamedFile::open(Path::new("../client/dist/styles").join(file))
+fn styles(file: PathBuf) -> CORS<io::Result<NamedFile>> {
+    CORS::any(NamedFile::open(Path::new("../client/dist/styles").join(file)))
 }
 
 // TODO: files like `fontawesome-webfont.eot?#iefix&v=4.7.0` are supported on master
@@ -42,11 +44,11 @@ struct V<'r> {
 
 #[allow(unused_variables)]
 #[get("/styles/<file..>?<v>")]
-fn styles_with_query(file: PathBuf, v: V) -> io::Result<NamedFile> {
-    NamedFile::open(Path::new("../client/dist/styles").join(file))
+fn styles_with_query(file: PathBuf, v: V) -> CORS<io::Result<NamedFile>> {
+    CORS::any(NamedFile::open(Path::new("../client/dist/styles").join(file)))
 }
 
 #[get("/images/<file..>")]
-fn images(file: PathBuf) -> io::Result<NamedFile> {
-    NamedFile::open(Path::new("../client/dist/images").join(file))
+fn images(file: PathBuf) -> CORS<io::Result<NamedFile>> {
+    CORS::any(NamedFile::open(Path::new("../client/dist/images").join(file)))
 }
