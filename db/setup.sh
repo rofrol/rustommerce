ed60a76b70bca4ea86121fd3c5476b8e51c5c4bc
Roman Frołow <rofrol@gmail.com>
2018-12-05 05:10:31 +0100

    /userInformation works

--- db/setup.sh
+++ db/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# You may need to create locale
+# $ localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
+# or just change command to use available locale
+# https://stackoverflow.com/questions/13115692/encoding-utf8-does-not-match-locale-en-us-the-chosen-lc-ctype-setting-requires/23273873#23273873
 initdb -D ~/pgdata -U postgres -A trust -E UTF8 --locale=en-US
 pg_ctl -D ~/pgdata -l ~/pg.log start
