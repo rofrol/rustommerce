c318ee7d1ad05af65cdc6c767fdf48411fd38a0f
Roman Frołow <rofrol@gmail.com>
2020-08-29 19:21:33 +0200

    db: locale that is present on ubuntu

--- db/start.sh
+++ db/start.sh
@@ -4,5 +4,5 @@
 # $ localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
 # or just change command to use available locale
 # https://stackoverflow.com/questions/13115692/encoding-utf8-does-not-match-locale-en-us-the-chosen-lc-ctype-setting-requires/23273873#23273873
-[ ! -d ~/pgdata ] && initdb -D ~/pgdata -U postgres -A trust -E UTF8 --locale=en-US
+[ ! -d ~/pgdata ] && initdb -D ~/pgdata -U postgres -A trust -E UTF8 --locale=en_US.utf8
 pg_ctl -D ~/pgdata -l ~/pg.log start
