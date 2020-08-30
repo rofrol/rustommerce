62b5a1f1e43af322a021e65afe05b72091aab4fc
Roman Frołow <rofrol@gmail.com>
2020-08-30 12:38:07 +0200

    db: create /var/run/postgresql

--- db/start.sh
+++ db/start.sh
@@ -5,4 +5,10 @@
 # or just change command to use available locale
 # https://stackoverflow.com/questions/13115692/encoding-utf8-does-not-match-locale-en-us-the-chosen-lc-ctype-setting-requires/23273873#23273873
 [ ! -d ~/pgdata ] && initdb -D ~/pgdata -U postgres -A trust -E UTF8 --locale=en_US.utf8
+
+# Whichever method you use to start it, that user (usually postgres) has
+# to own the /var/run/postgresql path and have write permissions.
+# https://postgrespro.com/list/thread-id/1561020
+[[ "$OSTYPE" == "linux-gnu" && ! -d /var/run/postgresql/ ]] && sudo install -d -m 0700 -o `id -u -n` -g `id -g -n` /var/run/postgresql/
+
 pg_ctl -D ~/pgdata -l ~/pg.log start
